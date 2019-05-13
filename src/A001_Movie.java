c7a05737e0fbfd5b574c043cc6f385e67b973de1
judith <judithmarialang@gmail.com>
2019-05-13 09:35:28 +0200

    first commit

---
+++ src/A001_Movie.java
@@ -0,0 +1,35 @@
+//A001: Create a class MovieTicket for a cineplex-like movie theater.
+
+//This class holds following information:
+//Price
+//Date
+//Time
+//Movie
+//cinema hall (1-10)
+
+//Create four  MovieTicket objects and store them in properties.
+
+//Create get and set methods accessing the movie ticket properties.
+
+//Now, calculate and store in a variable the total price of all created MovieObjects. Discuss with the group.
+
+
+public class A001_Movie {
+
+   public double price = NULL;
+   public String mydate  = NULL;
+   public String time = NULL;
+   public String movie = NULL;
+   public Integer cinemaHall = NULL;
+
+
+
+
+
+   public static void Main (String[] args){
+
+   }
+
+
+
+}
